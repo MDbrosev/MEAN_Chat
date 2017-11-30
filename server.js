e8af8ac687d4ec36c3445832d98c9ee9076c5a4c
Yoseph Hasson <23007047+MDbrosev@users.noreply.github.com>
2017-11-30 18:37:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,31 @@
 const mongo = require('mongodb').MongoClient;
 const client = require('socket.io').listen(4000).sockets;
-
-// Connect to mongo
+// Connect to mongodb
 mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
     if(err){
         throw err;
     }
-
     console.log('MongoDB connected...');
-
     // Connect to Socket.io
     client.on('connection', function(socket){
         let chat = db.collection('chats');
-
-        // Create function to send status
+        // Create function to emit status s
         sendStatus = function(s){
             socket.emit('status', s);
         }
-
         // Get chats from mongo collection
         chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
             if(err){
                 throw err;
             }
-
             // Emit the messages
             socket.emit('output', res);
         });
-
-        // Handle input events
+        // Handle our input events
         socket.on('input', function(data){
             let name = data.name;
             let message = data.message;
-
-            // Check for name and message
+            // Check for a name and message
             if(name == '' || message == ''){
                 // Send error status
                 sendStatus('Please enter a name and message');
@@ -41,7 +33,6 @@ mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
                 // Insert message
                 chat.insert({name: name, message: message}, function(){
                     client.emit('output', [data]);
-
                     // Send status object
                     sendStatus({
                         message: 'Message sent',
@@ -50,12 +41,11 @@ mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
                 });
             }
         });
-
-        // Handle clear
+        // Handle clear events
         socket.on('clear', function(data){
             // Remove all chats from collection
             chat.remove({}, function(){
-                // Emit cleared
+                // Emit cleared event
                 socket.emit('cleared');
             });
         });
